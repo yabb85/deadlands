01e4359354771e95b880a7a626d1b2c62dd4d602
Laurent Hory <yabb85@hory.me>
2018-09-14 22:37:48 +0200

    add redux persistence

--- deadlands/gui/app_js/app.js
+++ deadlands/gui/app_js/app.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { persistReducer, persistStore } from 'redux-persist'
+import immutableTransform from 'redux-persist-transform-immutable'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './components/App';
 
 import card_reduc from './redux/card_reduc'
@@ -9,21 +13,37 @@ import profil_reduc from './redux/profil_reduc'
 import assets_reduc from './redux/assets_reduc'
 import handicaps_reduc from './redux/handicaps_reduc'
 
+// declare liste des reducers
 const reducers = {
 	cards: card_reduc,
 	profil: profil_reduc,
 	assets: assets_reduc
 }
 
+// combine tous les reducers
 const reduc = combineReducers(reducers)
+
+// declare la sauvegarde du store
+const persistConfig = {
+	transforms: [immutableTransform()],
+	key: 'root',
+	storage: storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reduc)
+
 const store = createStore(
-	reduc,
+	persistedReducer,
 	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
+const persistor = persistStore(store)
+
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,
 	document.getElementById("deadlands")
 );
